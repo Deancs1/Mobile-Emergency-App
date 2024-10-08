cffca2480e8c883857a0ea77e445945c9714d6f5
Deancs1 <carmel.dean+GitHub@gmail.com>
2024-10-08 14:47:54 +0200

    mapview is showing, current location needs to be fixed

--- src/components/MedicalLocations.jsx
+++ src/components/MedicalLocations.jsx
@@ -1,13 +1,16 @@
+import React from "react";
+import MapView from "./MapView";
+
 const MedicalLocations = () => {
   return (
     <>
       <div className="bg-gray-800 p-4 min-h-screen flex flex-col items-center">
         <h1 className="text-white text-lg mb-4">Medical Locations</h1>
-        <div className="flex flex-col items-center p-4">
+        <div className="flex flex-col items-center p-4 w-full max-w-md">
           <div className="text-white">MapView will come here</div>
-          <img src="\src\images\mapview.png" />
+          <MapView />
         </div>
-        <div className="flex space-x-4 w-full max-w-md">
+        <div className="flex space-x-4 w-full max-w-md mt-4">
           <button className="bg-gray-700 text-white w-full p-4 rounded-lg">
             Hospitals
           </button>
